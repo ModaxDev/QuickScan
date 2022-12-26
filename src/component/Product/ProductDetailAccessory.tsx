5d71058413a5d416cec0f9cd109b9eedff0fefbc
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-12-27 00:09:32 +0100

    feat: add product detail accessory

---
+++ src/component/Product/ProductDetailAccessory.tsx
@@ -0,0 +1,62 @@
+import {Layout, Text} from "@ui-kitten/components";
+import {FlatList, Image, ScrollView, StyleSheet, TouchableOpacity, View} from "react-native";
+import {IMAGE_URL} from "../../utils/env";
+import React from "react";
+
+const ProductDetailAccessory = ({product} : any) => {
+
+    return (
+        <ScrollView>
+            <Layout style={styles.text}><Text category={"h6"}>Accéssoires / Gadgets</Text></Layout>
+            <FlatList horizontal data={product.productAccessories} renderItem={({item}: any) => {
+                return (
+                    <TouchableOpacity
+                        style={styles.container}
+                    >
+                        <Image
+                            style={{
+                                resizeMode: "contain",
+                                width: 90,
+                                height: 90,
+                                borderRadius: 5,
+                                backgroundColor: "#E5E5E5",
+                            }}
+                            source={{ uri: IMAGE_URL + item.fileUrl }}
+                        />
+                        <View
+                            style={{
+                                paddingHorizontal: 10,
+                                paddingVertical: 10,
+                                borderRadius: 12,
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: "#333",
+                                    fontWeight: "bold",
+                                    textAlign: "center",
+                                }}
+                            >
+                                {item.name}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                )
+            }}/>
+        </ScrollView>
+    )
+}
+
+export default ProductDetailAccessory
+
+const styles = StyleSheet.create({
+        container: {
+            marginLeft: 20,
+            textAlign: "center",
+        },
+    text: {
+        marginLeft: 20,
+        paddingBottom: 6,
+    }
+    })
+;
--- src/template/ProductScreen.tsx
+++ src/template/ProductScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Icon, Layout, Menu, Text} from "@ui-kitten/components";
 import {SafeScreen} from "../component/Display/SafeScreen";
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, FlatList, TouchableOpacity, Image, ScrollView} from "react-native";
 import ProductDetailHeader from "../component/Product/ProductDetailHeader";
 import ProductDetailHeaderInformation from "../component/Product/ProductDetailHeaderInformation";
 import ProductDetailDescription from "../component/Product/ProductDetailDescription";
+import {IMAGE_URL} from "../utils/env";
+import ProductDetailAccessory from "../component/Product/ProductDetailAccessory";
 
 const ProductScreen = ({route, navigation}: any) => {
     const {product} = route.params;
@@ -14,6 +16,7 @@ const ProductScreen = ({route, navigation}: any) => {
             <View style={styles.globalContainer}>
                 <ProductDetailHeaderInformation product={product}/>
                 <ProductDetailDescription product={product}/>
+                <ProductDetailAccessory product={product}/>
             </View>
         </SafeScreen>
     )
@@ -26,5 +29,9 @@ const styles = StyleSheet.create({
         globalContainer: {
             flex: 1,
         },
+    container: {
+        marginLeft: 20,
+        textAlign: "center",
+    },
     })
 ;
