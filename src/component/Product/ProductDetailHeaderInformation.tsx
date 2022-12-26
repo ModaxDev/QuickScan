bbe4759a74697c0e7a10e8ff09d70badb9a7c2cb
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-12-26 23:05:42 +0100

    feat: add product detail header information component

---
+++ src/component/Product/ProductDetailHeaderInformation.tsx
@@ -0,0 +1,69 @@
+import {Dimensions, Image, StyleSheet} from "react-native";
+import {IMAGE_URL} from "../../utils/env";
+import {Icon, Layout, Text} from "@ui-kitten/components";
+import React from "react";
+
+const ProductDetailHeaderInformation = ({product}:any) => {
+
+    return (
+        <Layout style={styles.description}>
+            <Image style={styles.image} source={{uri: IMAGE_URL + product.fileUrl}}/>
+            <Layout style={[styles.descriptionText, {flexDirection: "column", justifyContent: "space-evenly"}]}>
+                <Layout style={{flexDirection: "row"}}>
+                    <Text category="s1">Créer par {" "}</Text>
+                    <Layout style={{flexDirection: "row", justifyContent: "center"}}>
+                        <Image style={styles.iconCompany}
+                               source={{uri: IMAGE_URL + product.fileUrlCompanyLogo}}/>
+                        <Text style={{fontWeight: "bold"}}>{product.company}</Text>
+                    </Layout>
+                </Layout>
+                <Layout style={{flexDirection: "row", alignItems: "center"}}>
+                    <Icon style={styles.icon} fill={"#34d92e"} name="checkmark-outline"/>
+                    <Text category="s1">Réparable</Text>
+                </Layout>
+                <Layout style={{flexDirection: "row", alignItems: "center"}}>
+                    <Text category="s1">Indice de réparabilité</Text>
+                    <Text style={{fontWeight: "bold", color: product.reparabilityIndex && product.reparabilityIndex > 5 ? "#34d92e" : product.reparabilityIndex == 5 ? "#777777" : "#de2a2a" }}> {product.reparabilityIndex} / 10</Text>
+                </Layout>
+            </Layout>
+        </Layout>
+    )
+}
+
+export default ProductDetailHeaderInformation
+
+
+const styles = StyleSheet.create({
+        globalContainer: {
+            flex: 1,
+        },
+        container: {
+            margin: 20,
+        },
+        description: {
+            margin: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly', // align items to the left and right
+        }
+        ,
+        image: {
+            width: 80,
+            height: 80,
+            resizeMode: 'contain',
+        }
+        ,
+        iconCompany: {
+            width: 20,
+            height: 20,
+            resizeMode: 'contain',
+        }
+        ,
+        descriptionText: {
+            maxWidth: Dimensions.get('window').width - 120,
+        },
+        icon: {
+            width: 30,
+            height: 30,
+        }
+    })
+;
--- src/template/ProductScreen.tsx
+++ src/template/ProductScreen.tsx
@@ -1,30 +1,68 @@
 import React, {useEffect, useRef} from "react";
-import {Icon, Layout, Text} from "@ui-kitten/components";
+import {Icon, Layout, Menu, Text} from "@ui-kitten/components";
 import {SafeScreen} from "../component/Display/SafeScreen";
-import {View, StyleSheet, TouchableOpacity,} from "react-native";
+import {View, StyleSheet, TouchableOpacity, Image, useWindowDimensions, Dimensions} from "react-native";
 import {BlurView} from "expo-blur";
 import {IMAGE_URL} from "../utils/env";
-import * as Haptics from "expo-haptics";
-import Constants from "expo-constants";
 import ProductDetailHeader from "../component/Product/ProductDetailHeader";
+import RenderHTML from "react-native-render-html";
+import Constants from "expo-constants";
+import ProductDetailHeaderInformation from "../component/Product/ProductDetailHeaderInformation";
 
 const ProductScreen = ({route, navigation}: any) => {
-
+    const [selectedIndex, setSelectedIndex] = React.useState(null);
     const {product} = route.params;
+    const source = {
+        html: product.description,
+    };
     return (
         <SafeScreen>
             <ProductDetailHeader product={product}/>
             <View style={styles.globalContainer}>
-
+                <ProductDetailHeaderInformation product={product}/>
+                <Layout style={styles.container}>
+                    <RenderHTML contentWidth={100}
+                                source={source}/>
+                </Layout>
             </View>
         </SafeScreen>
     )
 }
 
+
 export default ProductScreen
 
 const styles = StyleSheet.create({
-    globalContainer: {
-        flex: 1,
-    },
-});
+        globalContainer: {
+            flex: 1,
+        },
+        container: {
+            margin: 20,
+        },
+        description: {
+            margin: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly', // align items to the left and right
+        }
+        ,
+        image: {
+            width: 80,
+            height: 80,
+            resizeMode: 'contain',
+        }
+        ,
+        iconCompany: {
+            width: 20,
+            height: 20,
+            resizeMode: 'contain',
+        }
+        ,
+        descriptionText: {
+            maxWidth: Dimensions.get('window').width - 120,
+        },
+        icon: {
+            width: 30,
+            height: 30,
+        }
+    })
+;
